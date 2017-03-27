c4da4aaead34920c7b32e45f268211d07916688e
Unknown <Jeppe_Skjerk_Bjerrum@hotmail.com>
2017-03-27 12:58:09 +0200

    logo size

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
 	<div class="header">
 		<ul>
-			<li><a href="index.php"><img src="images/receiver-x1-icon.png" alt="logo"></a></li>
+			<li><a href="index.php"><img src="images/receiver-x1-icon.png" height="50px" alt="logo"></a></li>
 			<li><a href="reciver.php">Recivers</a></li>
 			<li><a href="contact.php">Contact</a></li>
 		</ul>
