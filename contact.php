e0032c560f577a1ef018aeb6eb4d2f9182560d4c
Unknown <Jeppe_Skjerk_Bjerrum@hotmail.com>
2017-03-27 12:56:43 +0200

    Logo med mere

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hvem er vi? Seriøst! Hvem er vi?</title>
+<link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+<?php include 'footer.php'; ?>
+
+</body>
+</html>
