0b7df7ca72d5a33c5a1b0e7a3a9af46d32d3faa7
Unknown <Jeppe_Skjerk_Bjerrum@hotmail.com>
2017-03-27 12:45:39 +0200

    php include

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <header>
 	<div class="header">
 		<ul>
-			<li><a href="index.html">Home</a></li>
-			<li><a href="reciver.html">Recivers</a></li>
-			<li><a href="contact.html">contact</a></li>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="reciver.php">Recivers</a></li>
+			<li><a href="contact.php">contact</a></li>
 		</ul>
 	</div>
 </header>
