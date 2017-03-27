50f14d69451022df5a663c8322be0ac5e57f8a1f
Unknown <Jeppe_Skjerk_Bjerrum@hotmail.com>
2017-03-27 12:35:22 +0200

    Header.php lavet

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+	<div class="header">
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="reciver.html">Recivers</a></li>
+			<li><a href="contact.html">contact</a></li>
+		</ul>
+	</div>
+</header>
