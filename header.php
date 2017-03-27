2b33fd85dc43544d757aadcf07988d31a2d3af57
Unknown <Jeppe_Skjerk_Bjerrum@hotmail.com>
2017-03-27 13:26:09 +0200

    I dont know

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 	<div class="header">
 		<ul>
 			<li class="logo"><a href="index.php"><img src="images/receiver-x1-icon.png" height="50px" alt="logo"></a></li>
-			<li><a href="reciver.php">Recivers</a></li>
-			<li><a href="contact.php">Contact</a></li>
+			<li class="menu1"><a href="reciver.php">Recivers</a></li>
+			<li class="menu2"><a href="contact.php">Contact</a></li>
 		</ul>
 	</div>
 </header>
